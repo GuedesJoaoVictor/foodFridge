d266c32013a9999b0b0405af185d145add6a9555
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-01 17:09:24 -0300

    feat: Adicionando conexão com o banco de dados

--- src/main/java/dev/java10x/Fridge/FridgeApplication.java
+++ src/main/java/dev/java10x/Fridge/FridgeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FridgeApplication {
+public class  FridgeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FridgeApplication.class, args);
