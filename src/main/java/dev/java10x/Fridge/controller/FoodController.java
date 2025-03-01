c935dd89c7a6ee8b78c433ee534a7b1bbad1e2b1
João Víctor Guedes <joaoguedes1404@gmail.com>
2025-03-01 15:49:50 -0300

    feat: adicionando funcionalidade de listar e criar itens

--- src/main/java/dev/java10x/Fridge/controller/FoodController.java
+++ src/main/java/dev/java10x/Fridge/controller/FoodController.java
@@ -1,10 +1,21 @@
 package dev.java10x.Fridge.controller;
 
+import dev.java10x.Fridge.model.Food;
+import dev.java10x.Fridge.service.FoodService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/food")
 public class FoodController {
+    //Ingeção de dependencia.
+    private final FoodService foodService;
 
+    public FoodController(FoodService foodService) {
+        this.foodService = foodService;
+    }
+
+    @GetMapping
+    public List<Food> getAll() { return foodService.getAll()}
 }
--- src/main/java/dev/java10x/Fridge/service/FoodService.java
+++ src/main/java/dev/java10x/Fridge/service/FoodService.java
@@ -1,4 +1,22 @@
 package dev.java10x.Fridge.service;
 
+import dev.java10x.Fridge.model.Food;
+import dev.java10x.Fridge.repository.FoodRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class FoodService {
+    private final FoodRepository foodRepository;
+
+    public FoodService(FoodRepository foodRepository) {
+        this.foodRepository = foodRepository;
+    }
+
+    //Listar todos
+    public List<Food> getAll() { return foodRepository.findAll(); }
+
+    public Food save(Food food) { return foodRepository.save(food); }
+
 }
